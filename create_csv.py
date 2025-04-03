af4219f7237948107d49964179453e0776ef4338
Tom-Louis Friedrich <tlf.tofrie03@gmail.com>
2025-04-04 01:03:04 +0200

    created 10years dataset

--- create_csv.py
+++ create_csv.py
@@ -1,5 +1,5 @@
 import yfinance as yf
 
-df = yf.Ticker("BTC-EUR").history(period="5y")
+df = yf.Ticker("BTC-EUR").history(period="10y")
 
-df.to_csv('btc_historical_EUR.csv', index=True)
+df.to_csv('btc_historical_EUR_10y.csv', index=True)
